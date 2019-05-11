9333ce032b11b47d00d7ef58307aaf2eaabf859e
Lina <linamattsson@live.se>
2019-05-11 19:31:19 +0200

    added x and o and klicklistener

--- app/src/main/java/org/androidcourse/tictactoe/MainActivity.kt
+++ app/src/main/java/org/androidcourse/tictactoe/MainActivity.kt
@@ -1,12 +1,69 @@
 package org.androidcourse.tictactoe
 
+import android.content.Context
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.AttributeSet
+import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    private var field1IsFree = true
+    private var field2IsFree = true
+    private var field3IsFree = true
+    private var field4IsFree = true
+    private var field5IsFree = true
+    private var field6IsFree = true
+    private var field7IsFree = true
+    private var field8IsFree = true
+    private var field9IsFree = true
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onCreateView(name: String?, context: Context?, attrs: AttributeSet?): View? {
+
+
+
+
+
+        return super.onCreateView(name, context, attrs)
+    }
+
+    fun setClickListener(){
+        field1.setOnClickListener { if(field1IsFree) {field1.setBackgroundResource(R.drawable.x)
+        field1IsFree = false
+        doMove()}}
+        field2.setOnClickListener { if(field2IsFree) {field1.setBackgroundResource(R.drawable.x)
+        field2IsFree = false
+        doMove()}}
+        field3.setOnClickListener { if(field3IsFree) {field1.setBackgroundResource(R.drawable.x)
+        field3IsFree = false
+        doMove()}}
+        field4.setOnClickListener { if(field4IsFree) {field1.setBackgroundResource(R.drawable.x)
+        field4IsFree = false
+        doMove()}}
+        field5.setOnClickListener { if(field5IsFree) {field1.setBackgroundResource(R.drawable.x)
+        field5IsFree = false
+        doMove()}}
+        field6.setOnClickListener { if(field6IsFree) {field1.setBackgroundResource(R.drawable.x)
+        field6IsFree = false
+        doMove()}}
+        field7.setOnClickListener { if(field7IsFree) {field1.setBackgroundResource(R.drawable.x)
+        field7IsFree = false
+        doMove()}}
+        field8.setOnClickListener { if(field8IsFree) {field1.setBackgroundResource(R.drawable.x)
+        field8IsFree = false
+        doMove()}}
+        field9.setOnClickListener { if(field9IsFree) {field1.setBackgroundResource(R.drawable.x)
+        field9IsFree = false
+        doMove()}}
+    }
+    fun doMove(){
+        
     }
 }
